13d137d6afd5bdc127f95a66daf60bc311c23b85
lncodes <lastnightcodes@gmail.com>
2020-10-31 01:07:21 +0700

    Squashed 'PBO10K-10119907-Latihan18-Kelipatan/' content from commit a516652
    git-subtree-dir: PBO10K-10119907-Latihan18-Kelipatan
    git-subtree-split: a5166520f616941c58c59559db5b99e21a83123e

---
+++ src/main/java/PBO10K/NIM10119907/Latihan18/Kelipatan/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119907.Latihan18.Kelipatan;
+
+/**
+ *
+ * @author
+ * Nama     : T Doni
+ * Kelas    : IF10K
+ * NIM      : 10119907
+ * Deskripsi Program : Program ini digunakan untuk menampilkan kelipatan
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float kelipatan = 3.5f;
+        for (float i = kelipatan; i <= 35; i+=kelipatan) 
+            System.out.println(i);
+    }
+}
